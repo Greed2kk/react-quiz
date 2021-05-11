babced7906cf9c2f3ab6d162a72eaa594d0c50ec
greedkk <draiver78@mail.ru>
2021-05-11 20:01:25 +0300

    auth Firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import openNotification from 'components/UI/Notification/Notification'
 import classes from './Auth.module.scss'
 import MyButton from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
@@ -39,9 +41,45 @@ class Auth extends Component {
     },
   }
 
-  loginHandler = () => {}
+  loginHandler = async () => {
+    // eslint-disable-next-line react/destructuring-assignment
+    const { email, password } = this.state.formControls
+    const authData = {
+      email: email.value,
+      password: password.value,
+      returnSecureToken: true,
+    }
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDgIGORo1c1k8otoW5U74hBMp_WK1Hx0XE`,
+        authData
+      )
+      openNotification('success', response.statusText)
+    } catch (error) {
+      openNotification(error.name, error.message)
+      console.warn(error)
+    }
+  }
 
-  registerHandler = () => {}
+  registerHandler = async () => {
+    // eslint-disable-next-line react/destructuring-assignment
+    const { email, password } = this.state.formControls
+    const authData = {
+      email: email.value,
+      password: password.value,
+      returnSecureToken: true,
+    }
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDgIGORo1c1k8otoW5U74hBMp_WK1Hx0XE`,
+        authData
+      )
+      openNotification('success', response.statusText)
+    } catch (error) {
+      openNotification(error.name, error.message)
+      console.warn(error)
+    }
+  }
 
   submitHandler = e => {
     e.preventDefault()
